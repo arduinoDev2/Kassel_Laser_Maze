2717654e2991c7250e29758c0a89b772388a7e7a
arduinoDev2 <aggalex.com>
2024-04-30 11:26:02 +0300

    added button for fogmachine on base, tested ok. needs update for diagnosis to set times on eeprom

--- Laser_Maze_Base_Station_Agent/Laser_Maze_Base_Station_Agent.ino
+++ Laser_Maze_Base_Station_Agent/Laser_Maze_Base_Station_Agent.ino
@@ -3,6 +3,7 @@
    FINAL STATION - COM4
 */
 #include <CubeRoomAgent.h>
+#include <TimerOne.h>
 
 char roomName[] = "LASER_MAZE";  // MySQL room name
 
@@ -17,16 +18,28 @@ const int greenLightMillis = 10000;
 
 const int diagnosisTestBtn = 47;
 
+const int fogTime = 5000;
+const long dissableFogTime = 300000;
+//const long dissableFogTime = 10000;
+#define FOG_MACHINE_PIN 9
+#define FOG_BUTTON_PIN 43
+unsigned long startFogTime;
+unsigned long startDiasableTime;
+bool fogMachineEnabled = 1;
+bool isFogMachineOn = 0;
+
 TimerEvent agentLoop;
 
 //put games varieables here
-const int rfidPins[]  = {18, 19, 23, 25, 27, 29};
-const int winScore[]  = {100, 300, 500};
+const int rfidPins[] = { 18, 19, 23, 25, 27, 29 };
+const int winScore[] = { 100, 300, 500 };
 const int smokePin = A0;
 void setup() {
   Serial.begin(9600);
+  Timer1.attachInterrupt(isr, 100000); // Set the desired interval (in microseconds)
   pinMode(smokePin, OUTPUT);
   digitalWrite(smokePin, HIGH);
+  pinMode(FOG_MACHINE_PIN, OUTPUT);
   Serial.println("Base Board - COM5");
   diagnosisSetup();
   agentLoop.set(1000, updateDataFromAgent);
@@ -57,13 +70,11 @@ void loop() {
         score = startGame(dif, numberOfPlayers, gameTime);  //start the game, wait the game to finish
 
         if (score == 0) {
-          room.finishLose(timeRem);                         //lose, score = 0
-        }
-        else if (score == -1) {
-          room.finishTimeout();                             //timeout, score = 0
-        }
-        else {
-          room.finishWin(score, timeRem);    //win and post score
+          room.finishLose(timeRem);  //lose, score = 0
+        } else if (score == -1) {
+          room.finishTimeout();  //timeout, score = 0
+        } else {
+          room.finishWin(score, timeRem);  //win and post score
         }
       }
     }
@@ -99,8 +110,7 @@ int startGame(int difficulty, int numberOfPlayers, int gameTime) {
 
         green = !green;
       }
-    }
-    else {
+    } else {
       room.lightCube();
       digitalWrite(5, LOW);
     }
@@ -134,12 +144,34 @@ int startGame(int difficulty, int numberOfPlayers, int gameTime) {
     if (counter == numberOfPlayers) {
       Serial.println("RFID");
       timeRem = round((timeoutTime - millis()) / 1000);
-      return winScore[difficulty - 1]; //win
+      return winScore[difficulty - 1];  //win
     }
 
     agentLoop.update();
     delay(50);
   }
   timeRem = 0;
-  return -1; //timeout
+  return -1;  //timeout
+}
+
+void isr() {
+  if (fogMachineEnabled) {
+    if (digitalRead(FOG_BUTTON_PIN)) {
+      digitalWrite(FOG_MACHINE_PIN, 1);
+      isFogMachineOn = 1;
+      startDiasableTime = millis();
+      startFogTime = millis();
+      fogMachineEnabled = 0;
+    }
+  } else {
+    if (isFogMachineOn) {
+      if (millis() - startFogTime > fogTime) {
+        digitalWrite(FOG_MACHINE_PIN, 0);
+        isFogMachineOn = 0;
+      }
+    }
+    if (millis() - startDiasableTime > dissableFogTime) {
+      fogMachineEnabled = 1;
+    }
+  }
 }
